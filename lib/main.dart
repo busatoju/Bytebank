b3563e64553b792c3a91cda45fe02422e5c867ef
busatoju <busatoju@gmail.com>
2021-02-20 11:22:15 -0300

    Implementando Cards de Transferências

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+
     home: Scaffold(
       appBar: AppBar(
           title: Text('Transferências')
       ),
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('R\$ 100.00'),
+                subtitle: Text('27665843'),
+              )
+            ),
+            Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('R\$ 200.00'),
+                  subtitle: Text('27665843'),
+                )
+            ),
+          ],
+        ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
       ),
