81c200a7eef741f031e4471cdf7e17a2c2681753
busatoju <busatoju@gmail.com>
2021-02-20 12:02:19 -0300

    Código refatorado, criada ListaTransferências, ItemTransferência e Transferência

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,49 @@ void main() => runApp(MaterialApp(
       appBar: AppBar(
           title: Text('Transferências')
       ),
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('R\$ 100.00'),
-                subtitle: Text('27665843'),
-              )
-            ),
-            Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('R\$ 200.00'),
-                  subtitle: Text('27665843'),
-                )
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
       floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
           child: Icon(Icons.add),
       ),
     ),
 ));
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Column(
+     children: <Widget>[
+       ItemTransferencia(Transferencia(100,27665843)),
+       ItemTransferencia(Transferencia(300,27665843)),
+       ItemTransferencia(Transferencia(200,27665843)),
+     ],
+   );
+  }
+
+}
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  const Transferencia(this.valor, this.numeroConta);
+}
