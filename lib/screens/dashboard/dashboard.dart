9fa8515a696a4ff96e593f6e9cdd27784402e011
busatoju <busatoju@gmail.com>
2021-02-27 16:14:40 -0300

    Adicionado o botão nova transferência  e transferências a tela inicial

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -2,6 +2,8 @@
 import 'package:bytebank/models/balance.dart';
 import 'package:bytebank/screens/dashboard/balance_card.dart';
 import 'package:bytebank/screens/deposit/form.dart';
+import 'package:bytebank/screens/transfer/forms.dart';
+import 'package:bytebank/screens/transfer/list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,19 +20,49 @@ class Dashboard extends StatelessWidget{
               alignment: Alignment.topCenter,
               child: BalanceCard(),
             ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: const Text('Receber depósito'),
+                  color: Colors.green,
+                  onPressed: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context){
+                          return FormWithdraw();
+                        })
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: const Text('Nova transferência'),
+                  color: Colors.green,
+                  onPressed: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context){
+                          return TransferForm();
+                        })
+                    );
+                  },
+                )
+              ],
 
+            ),
             RaisedButton(
-              child: const Text('Receber depósito'),
+              child: const Text('Transferências'),
+              color: Colors.green,
               onPressed: (){
-                /*balance.add(10);*/
                 Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context){
-                      return FormWithdraw();
+                      return TransferList();
                     })
                 );
               },
             )
+
           ],
         ));
   }
